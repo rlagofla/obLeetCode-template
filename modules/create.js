1dadee7bea880a03656d9cc5f095da780c1ab89a
rlagofla <miss.pinky.0825@gmail.com>
2025-04-16 14:45:56 +0800

    创建后自动打开

--- modules/create.js
+++ modules/create.js
@@ -1,8 +1,14 @@
 import fs from "fs";
 import path from "path";
+import open from 'open';
 
 import { fetchLeetCode, generateMarkdown } from "./utils.js"
 
+async function openObsidianLink(vault, filePath) {
+    const encodedUri = `obsidian://open?vault=${encodeURIComponent(vault)}&file=${encodeURIComponent(filePath)}`;
+    // console.log(encodedUri)
+    await open(encodedUri);
+}
 
 export const createMd = async (url, dir) => {
     const info = await fetchLeetCode(url)
@@ -13,4 +19,6 @@ export const createMd = async (url, dir) => {
     fs.writeFileSync(filePath, markdownContent, "utf-8");
 
     console.log(`✅ 已生成笔记`);
+
+    // await openObsidianLink("算法", `repo/${info.titleCn}.md`);
 }
